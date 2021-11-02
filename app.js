95a4eec2dcd98debf6ba5c675f754f3f821bdc2f
Sevda Avcılar <sevdavcilar@gmail.com>
2021-11-02 15:42:53 +0300

    controller added

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const enj = require('ejs');
 const postData = require('./models/postData');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const postController = require('./controllers/postController');
 
 const app = express();
 
@@ -18,14 +20,18 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  }),
+);
 
 //Routes
-app.get('/', async (req, res) => {
-  const allposts = await postData.find();
-  res.render('index', {
-    allposts,
-  });
-});
+app.get('/', postController.getAllPosts);
+app.get('/allposts/:id', postController.getPost);
+app.post('/add_post', postController.createPost);
+app.put('/allposts/:id', postController.updatePost);
+app.delete('/allposts/:id', postController.deletePost);
 
 app.get('/about', (req, res) => {
   res.render('about');
@@ -35,16 +41,11 @@ app.get('/add_post', (req, res) => {
   res.render('add_post');
 });
 
-app.get('/allposts/:id', async (req, res) => {
-  const allp = await postData.findById(req.params.id);
-  res.render('post', {
+app.get('/allposts/edit/:id', async (req, res) => {
+  const allp = await postData.findOne({ _id: req.params.id });
+  res.render('edit', {
     allp,
   });
-});
-
-app.post('/add_post', async (req, res) => {
-  await postData.create(req.body);
-  res.redirect('/');
 });
 
 app.listen(5000, () => {
